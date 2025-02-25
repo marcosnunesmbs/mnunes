bf16cd80f94bbb78955f2159aeb484ec76cf7360
Marcos Nunes <marcosnunesmbs@gmail.com>
2025-02-25 16:42:50 -0300

    add certifications and skills data to JavaScript files

---
+++ assets/js/certifications.js
@@ -0,0 +1,17 @@
+const certifications = [
+    {
+        name: "ITIL Foundation",
+        imgSrc: "./assets/img/badges/itil.png",
+        alt: "ITIL Foundation Badge"
+    },
+    {
+        name: "Scrum",
+        imgSrc: "./assets/img/badges/scrum.png",
+        alt: "Scrum Badge"
+    },
+    {
+        name: "AI-900",
+        imgSrc: "./assets/img/badges/ai900.png",
+        alt: "AI-900 Badge"
+    }
+];
---
+++ assets/js/projects.js
@@ -0,0 +1,52 @@
+// assets/js/projects.js
+const projects = [
+    {
+        name: "ChefIA",
+        url: "https://chefia.mnunes.xyz/",
+        imgSrc: "https://chefia.mnunes.xyz/chef-ia-logo.png",
+        alt: "Logo ChefIA",
+        description: "A Recipe Generator Application made with AI to suggest recipes based on available ingredients. Using Gemini API, Tailwind, NodeJS, Google Gemini and GCP."
+    },
+    {
+        name: "My Next Read",
+        url: "https://mynext.mnunes.xyz/",
+        imgSrc: "https://mynext.mnunes.xyz/resource/icon.png",
+        alt: "My Next",
+        description: "A AI Application made with Gemini API to recommend Books and Movies based on your like. Using Bootstrap 5, NodeJS, Google Gemini and GCP."
+    },
+    {
+        name: "Coinpaprika BR Bot",
+        url: "https://github.com/marcosnunesmbs/coinpaprika-br-bot",
+        imgSrc: "./assets/img/logos/cppbr.jpg",
+        alt: "Coinpaprika BR Bot",
+        description: "A Telegram bot to consult cryptocurrency prices and other things using the Coinparika API. <a href='https://t.me/cppbr_bot' target='_blank'>Access Bot</a>"
+    },
+    {
+        name: "WpCreator",
+        url: "https://github.com/marcosnunesmbs/wpcreator/",
+        imgSrc: "./assets/img/logos/wpcreator.png",
+        alt: "Logo Glaucia PSI",
+        description: "A Open Source PHP CLI to create Wordpress Custom Post Types, Elementor's Widgets and another utilityes by yaml/json files."
+    },
+    {
+        name: "Devorando.dev",
+        url: "https://devorando.dev/",
+        imgSrc: "https://devorando.dev/wp-content/uploads/2024/05/dev-4.png",
+        alt: "Logo Devorando.dev",
+        description: "A Youtube Channel website made from me. Using Elementor, Wordpress, Customize Theme, Self made Plugins and VPS Cloud."
+    },
+    {
+        name: "Glaucia PSI",
+        url: "https://glauciapsi.com/",
+        imgSrc: "https://glauciapsi.com/wp-content/uploads/2022/05/LOGO-lotus.png",
+        alt: "Logo Glaucia PSI",
+        description: "A Psicologist Website made with Wordpress, Elementor Owner Widgets, Customized Theme anda VPS Cloud."
+    },
+    {
+        name: "O Rafa é Massa",
+        url: "https://orafaemassa.com.br/",
+        imgSrc: "https://orafaemassa.com.br/wp-content/uploads/2020/07/orafaemassa-sm.png",
+        alt: "Logo Glaucia PSI",
+        description: "A E-commerce Website made with Wodpress and Woocommerce. Using Elementor page builder and a VPS Cloud."
+    },
+];
