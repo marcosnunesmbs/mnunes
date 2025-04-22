6887261496a6c8f22fcb759a400da66eb3da4dfc
Marcos Nunes <marcosnunesmbs@gmail.com>
2025-04-22 17:48:25 -0300

    Refactor code structure for improved readability and maintainability

---
+++ new-site/assets/js/certifications.js
@@ -0,0 +1,23 @@
+const certifications = [
+    {
+        name: "ITIL Foundation",
+        imgSrc: "./assets/img/badges/itil.png",
+        alt: "ITIL Foundation Badge"
+    },
+    {
+        name: "Scrum",
+        imgSrc: "./assets/img/badges/scrum.png",
+        alt: "Scrum Badge"
+    },
+    {
+        name: "AI-900",
+        imgSrc: "./assets/img/badges/ai900.png",
+        alt: "AI-900 Badge"
+    },
+    ,
+    {
+        name: "GitHub Copilot",
+        imgSrc: "./assets/img/badges/copilot.png",
+        alt: "GitHub Copilot Badge"
+    }
+];
---
+++ new-site/assets/js/skills.js
@@ -0,0 +1,95 @@
+const skills = [
+    {
+        name: "Laravel",
+        icon: "fa-laravel",
+        color: "bg-orange-600"
+    },
+    {
+        name: "Node.js",
+        icon: "fa-node-js",
+        color: "bg-green-600"
+    },
+    {
+        name: "Angular",
+        icon: "fa-angular",
+        color: "bg-red-600"
+    },
+    {
+        name: "Vue.js",
+        icon: "fa-vuejs",
+        color: "bg-teal-600"
+    },
+    {
+        name: "Git",
+        icon: "fa-git",
+        color: "bg-orange-600"
+    },
+    {
+        name: "Git Hub",
+        icon: "fa-github",
+        color: "bg-gray-600"
+    },
+    {
+        name: "PHP",
+        icon: "fa-php",
+        color: "bg-indigo-600"
+    },
+    {
+        name: "WordPress",
+        icon: "fa-wordpress",
+        color: "bg-cyan-700"
+    },
+    {
+        name: "Elementor",
+        icon: "fa-elementor",
+        color: "bg-pink-600"
+    },
+    {
+        name: "Python",
+        icon: "fa-python",
+        color: "bg-blue-700"
+    },
+    {
+        name: "HTML5",
+        icon: "fa-html5",
+        color: "bg-orange-600"
+    },
+    {
+        name: "CSS3",
+        icon: "fa-css3",
+        color: "bg-blue-600"
+    },
+    {
+        name: "JavaScript",
+        icon: "fa-js",
+        color: "bg-yellow-500"
+    },
+    {
+        name: "Docker",
+        icon: "fa-docker",
+        color: "bg-blue-600"
+    },
+    {
+        name: "Linux",
+        icon: "fa-linux",
+        color: "bg-pink-600"
+    },
+    {
+        name: "Google Cloud",
+        icon: "google_cloud",
+        image: "https://www.vectorlogo.zone/logos/google_cloud/google_cloud-icon.svg",
+        color: "bg-gray-800"
+    },
+    {
+        name: "MySQL",
+        icon: "mysql",
+        image: "https://www.vectorlogo.zone/logos/mysql/mysql-official.svg",
+        color: "bg-gray-800"
+    },
+    {
+        name: "MongoDB",
+        icon: "mongodb",
+        image: "https://www.vectorlogo.zone/logos/mongodb/mongodb-icon.svg",
+        color: "bg-gray-800"
+    }
+];
