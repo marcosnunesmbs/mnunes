2a15b34c1b2a8e6ca383d3196a587bec468633e8
Marcos Nunes <marcosnunesmbs@gmail.com>
2026-03-11 22:41:46 -0300

    Update project link text from 'Visitar' to 'View Project' for consistency

--- site-2026/assets/js/main.js
+++ site-2026/assets/js/main.js
@@ -124,7 +124,7 @@ function renderProjects() {
                 </div>
                 <p class="text-base-400 text-sm leading-relaxed flex-1">${project.description}</p>
                 <a href="${project.url}" target="_blank" rel="noopener noreferrer" class="project-link mt-auto">
-                    Visitar
+                    View Project
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
                         <path fill-rule="evenodd" d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z" clip-rule="evenodd"/>
                     </svg>
